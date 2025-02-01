a554e70ff332830f21acb905c1c3545415fc6f7d
eddyw <ewanli11@srrcc.org.uk>
2025-02-01 21:45:45 +0000

    Created button class

--- button.py
+++ button.py
@@ -1,14 +1,28 @@
 import pygame
-
-# colours
-white = (255, 255, 255)
-black = (0, 0, 0)
-light_blue = (0, 51, 102)
-
 # button class
 class Button:
-    def __init__(self, text, x, y, width, height, action = None):
+    def __init__(self, text, x, y, width, height, colour):
         self.text = text
-        self.rect = pygame.Rect(x, y, width, height)
-        self.action = action
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
+        self.button = pygame.Rect(x, y, width, height)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen,"light grey",self.button, 0, 5)
+        pygame.draw.rect(screen,"light grey",self.button, 5, 5)
+        font = pygame.font.Font(None, 36)
+        text = font.render(self.text, True, (255,255,255))
+        screen.blit(text, [self.x + 15, self.y + 7])
+
+    def check_clicked(self):
+        if self.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+            return True
+        else:
+            return False
+
+
+
 
