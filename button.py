ecc6e598f9447abdabc4df47ba0717c5fce54b13
eddyw <ewanli11@srrcc.org.uk>
2025-02-03 00:11:34 +0000

    Made major changes to button class, amended it as well as made methods to check button hovers, button clicks and to draw the button.

--- button.py
+++ button.py
@@ -1,24 +1,35 @@
 import pygame
 # button class
 class Button:
-    def __init__(self, text, x, y, width, height, colour):
+    def __init__(self, text, x, y, font, screen, colour):
         self.text = text
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.font = font
+        self.screen = screen
         self.colour = colour
-        self.button = pygame.Rect(x, y, width, height)
+        self.rect = pygame.Rect(self.x, self.y, 200, 50)
 
-    def draw(self, screen):
-        pygame.draw.rect(screen,"light grey",self.button, 0, 5)
-        pygame.draw.rect(screen,"light grey",self.button, 5, 5)
-        font = pygame.font.Font(None, 36)
-        text = font.render(self.text, True, (255,255,255))
-        screen.blit(text, [self.x + 15, self.y + 7])
+    def draw(self):
+        button_text = self.font.render(self.text, True, "Black")
+        text_rect = button_text.get_rect(center=self.rect.center)
+        if self.check_hover():
+            pygame.draw.rect(self.screen, "dark grey", self.rect, 0, 5)
+        else:
+            pygame.draw.rect(self.screen, self.colour, self.rect, 0, 5)
+        pygame.draw.rect(self.screen, "black", self.rect, 2, 5)
+        self.screen.blit(button_text, text_rect)
+
+    def check_hover(self):
+        mouse_pos = pygame.mouse.get_pos()
+        button_rect = pygame.Rect((self.x, self.y), (175,35))
+        return button_rect.collidepoint(mouse_pos)
 
     def check_clicked(self):
-        if self.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+        mouse_pos = pygame.mouse.get_pos()
+        left_click = pygame.mouse.get_pressed()[0]
+        button_rect = pygame.Rect((self.x, self.y), (175, 35))
+        if left_click and button_rect.collidepoint(mouse_pos):
             return True
         else:
             return False
