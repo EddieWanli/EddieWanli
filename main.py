c6cd5f7ce4408c3a7448a8dbccccd5b0146e1c62
eddyw <ewanli11@srrcc.org.uk>
2025-02-03 00:14:54 +0000

    Reorganised the structure, created buttons for the main menu and a game state system, allowing each state to be worked on independently within the mian game loop. Also added a game screen system, allowing different screens to be shown after its respective button is pressed in the main menu. Made the exit button exit the game upon left click as well as pressing Esc. Pressing esc in either progress or settings screen now brings you back to the main menu, with an additional note on the top left as a guide on how to do this. I have represented the different game screens with different colours at the moment, progress screen has two boxes to represent enemy kills and time elapsed, still a WIP.

--- main.py
+++ main.py
@@ -1,41 +1,89 @@
-# initialize pygame modules
+# initialize useful modules
+import sys
 import pygame
 from button import Button
 pygame.init()
 
- 
 # create game window
-width = 1000
-height = 750
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((1000, 750))
 pygame.display.set_caption("Terraworld")
 
-# main menu control
-main_menu = False
-
-# time controls
+# clock controls
 fps = 60
 timer = pygame.time.Clock()
 
-# font
-font = pygame.font.SysFont("freesansbold.ttf", 30)
+# game text
+font = pygame.font.Font("retro.ttf", 50)
+font2 = pygame.font.Font(None, 35)
+title_text = font.render("TERRAWORLD", True, "black")
+title_text_rect = title_text.get_rect(midbottom=(500, 100))
+back_to_menu = font2.render("Esc - Main Menu", True, "black")
 
-def draw_game():
-    button = Button("Main menu", 370, 450, 230, 450)
-    button.draw()
+# default game state
+game_state = "menu"
+
+# buttons
+new_game_button = Button("New Game",412.5,250, font2,  screen, (150,150,150))
+progress_button = Button("Progress",412.5,310, font2, screen, (175,175,175))
+settings_button = Button("Settings",412.5,370, font2, screen, "light grey")
+exit_button = Button("Exit",412.5,450, font2, screen, (250,50,87))
 
 # game loop
-run = True
-while run:
-    screen.fill("light blue")
-    timer.tick(fps)
-
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                if game_state == "settings" or game_state == "progress":
+                    game_state = "menu"
+                elif game_state == "menu":
+                    pygame.quit()
+                    sys.exit()
+        # button click checks
+        if game_state == "menu":
+            if new_game_button.check_clicked():
+                game_state = "game"
+            if settings_button.check_clicked():
+                game_state = "settings"
+            if progress_button.check_clicked():
+                game_state = "progress"
+            if exit_button.check_clicked():
+                pygame.quit()
+                sys.exit()
 
-    pygame.display.flip()
-pygame.quit()
+    # game screen rendering
+    if game_state == "menu":
+        screen.fill("light blue")
+        screen.blit( title_text , title_text_rect)
+        new_game_button.draw()
+        settings_button.draw()
+        progress_button.draw()
+        exit_button.draw()
+
+    elif game_state == "game":
+        screen.fill("green")
+        # add gameplay code here
+
+    elif game_state == "settings":
+        screen.fill("dark grey")
+        screen.blit(back_to_menu, (0, 0))
+
+        # add settings logic here
+
+    elif game_state == "progress":
+        screen.fill("yellow")
+        screen.blit(back_to_menu, (0, 0))
+        pygame.draw.rect(screen, "light grey", pygame.Rect(283, 300, 100, 50), 0,5)
+        pygame.draw.rect(screen, "black", pygame.Rect(283, 300, 100, 50), 2,5)
+        pygame.draw.rect(screen, "light grey", pygame.Rect(616, 300, 100, 50), 0, 5)
+        pygame.draw.rect(screen, "black", pygame.Rect(616, 300, 100, 50), 2, 5)
+        # add progress logic here
+
+    pygame.display.update()
+    timer.tick(fps)
+
 
 
 
